21f92a83a737d70d4552134c2971bec37b25b3d4
ranjnagemini <ranjnagemini@gmail.com>
2023-05-06 15:09:24 +0530

    updates to readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # azure-resume
-My own profile as a part of Cloud Resume challenge
+My own profile as a part of Cloud Resume challenge [ACG project video](https://www.youtube.com/watch?v=A-s6rZVMytc)
+
+##First steps
+
+- Frontend folder contains the website
+- main.js contains visitor counter code
+
+
+
+```js
+const
+```
--- frontend/main.js
+++ frontend/main.js
@@ -12,7 +12,7 @@ const getVisitCount = () => {
     fetch(functionApi).then (response =>{
         return response.json()
         }).then(response =>{
-            console.log(Website called function API."");
+            console.log("Website called function API.");
             count = response.count;
             document.getElementById("counter").innerText = count;
         }).catch(function(error){
